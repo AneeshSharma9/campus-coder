673f89bec09e2ebaf7eb120e3b8ab619f5eb2d94
AneeshSharma9 <ansh993@gmail.com>
2023-08-02 17:11:40 -0400

    styling login and signup pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,50 +1,100 @@
 import React, { useContext, useState } from "react";
-import { AccountContext } from "./Account"
+import { AccountContext } from "./Account";
 /* import UserPool from "../UserPool"
 import {CognitoUser, AuthenticationDetails} from "amazon-cognito-identity-js"; */
 
-
 const Login = () => {
-
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
-
 
     const { authenticate } = useContext(AccountContext);
 
     const onSubmit = (event) => {
         event.preventDefault();
-        authenticate(username, password).then(data => {
-            console.log("Logged in!", data);
-            window.location.href = "/";
-
-        }).catch(err => {
-            console.log("Failed to login", err);
-            setError(err);
-        });
+        authenticate(username, password)
+            .then((data) => {
+                console.log("Logged in!", data);
+                window.location.href = "/";
+            })
+            .catch((err) => {
+                console.log("Failed to login", err);
+                setError(err);
+            });
     };
 
     return (
-        <div class="container">
-            <h1 class="customh1">Log in</h1>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input class="form-control" id="usernameInput" aria-describedby="usernameHelp" placeholder="Enter username" value={username} onChange={(event) => setUsername(event.target.value)} />
+        <div>
+            <section>
+                <div class="container py-5 h-100">
+                    <div class="row d-flex justify-content-center align-items-center h-100">
+                        <div class="col col-xl-10">
+                            <div class="card innerlogincard">
+                                <div class="row g-0">
+                                    <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                        <img
+                                            src="https://images.unsplash.com/photo-1536148935331-408321065b18?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                                            alt="login form"
+                                            class="img-fluid loginimage"
+                                        />
+                                    </div>
+                                    <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                        <div class="card-body text-black pr-5">
+                                            <form onSubmit={onSubmit}>
+                                                <div class="d-flex align-items-center mb-3 pb-1">
+                                                    <i class="fas fa-cubes fa-2x me-3"></i>
+                                                    <span class="customh1 mb-0">Campus Coder</span>
+                                                </div>
+
+                                                <h5 class="fw-normal mb-3 pb-3 login-spacing">Sign into your account</h5>
+
+                                                <div class="form-outline mb-4">
+                                                    <input
+                                                        type="username"
+                                                        id="usernameInput"
+                                                        class="form-control form-control-lg"
+                                                        placeholder="Username"
+                                                        value={username}
+                                                        onChange={(event) => setUsername(event.target.value)}
+                                                    />
+                                                </div>
+
+                                                <div class="form-outline mb-4">
+                                                    <input
+                                                        type="password"
+                                                        id="passwordInput"
+                                                        class="form-control form-control-lg"
+                                                        placeholder="Password"
+                                                        value={password}
+                                                        onChange={(event) => setPassword(event.target.value)}
+                                                    />
+                                                </div>
+                                                {error && (
+                                                    <div className="alert alert-danger" role="alert">
+                                                        {error.message}
+                                                    </div>
+                                                )}
+                                                <div class="pt-1 mb-4">
+                                                    <button class="primary-button login-button-shadow btn btn-dark btn-lg btn-block" type="submit">Login</button>
+                                                </div>
+                                                <a class="small text-muted" href="/login">
+                                                    Forgot password?
+                                                </a>
+                                                <p class="">
+                                                    Don't have an account?{" "}
+                                                    <a href="signUp" class="small-link">
+                                                        Register here
+                                                    </a>
+                                                </p>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input class="form-control" id="passwordInput" aria-describedby="passwordHelp" placeholder="Enter password" value={password} onChange={(event) => setPassword(event.target.value)} />
-                </div>
-                <button class="login-button btn" type="submit" >Login</button>
-            </form>
-            <a class="createacc" href="signUp">Create an account</a>
-            {error && ( 
-                <div className="alert alert-danger" role="alert">
-                    {error.message}
-                </div>
-            )}
+            </section>
         </div>
     );
 };
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,57 +6,95 @@ const Signup = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
-    const [phoneNum, setPhoneNum] = useState("");
-    const [name, setName] = useState("");
+    const [phonenum, setPhoneNum] = useState("");
+    const [firstname, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
     const [error, setError] = useState(null);
 
     const onSubmit = (event) => {
         event.preventDefault();
 
-        UserPool.signUp(username, password, [{ 'Name': 'name', 'Value': name }, { 'Name': 'email', 'Value': email }, { 'Name': 'phone_number', 'Value': phoneNum }], null, (err, data) => {
+        UserPool.signUp(username, password, [{ 'Name': 'name', 'Value': (firstname + " " + lastname) }, { 'Name': 'email', 'Value': email }, { 'Name': 'phone_number', 'Value': phonenum }], null, (err, data) => {
             if (err) {
                 console.error(err);
                 setError(err);
             } else {
                 console.log(data);
-                //window.location.href = "login";
+                window.location.href = "./login";
             }
         }
         );
     };
 
     return (
-        <div class="container">
-            <h1>Sign Up</h1>
-            <form onSubmit={onSubmit}>
-                <div class="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input class="form-control" id="nameInput" aria-describedby="nameHelp" placeholder="Enter name" value={name} onChange={(event) => setName(event.target.value)} />
+        <div>
+            <section class="text-center text-lg-start">
+
+                <div class="container py-4">
+                    <div class="row g-0 align-items-center">
+                        <div class="col-lg-6 mb-5 mb-lg-0 signupcard-border">
+                            <div class="card cascading-right cas-right-card" >
+                                <div class="card-body p-5 shadow-5 text-center">
+                                    <h2 class="customh1 fw-bold mb-5">Sign up now</h2>
+                                    <form onSubmit={onSubmit}>
+                                        <div class="row">
+                                            <div class="col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="form3Example1" class="form-control" placeholder="First name" value={firstname} onChange={(event) => setFirstName(event.target.value)} />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="form3Example2" class="form-control" placeholder="Last name" value={lastname} onChange={(event) => setLastName(event.target.value)} />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="email" id="form3Example3" class="form-control" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)}/>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="phonenum" id="form3Example9" class="form-control" placeholder="Phone number (include country code)" value={phonenum} onChange={(event) => setPhoneNum(event.target.value)}/>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="username" id="form3Example9" class="form-control" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)}/>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="password" id="form3Example4" class="form-control" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                                        </div>
+
+                                        {error && ( 
+                                            <div className="alert alert-danger" role="alert">
+                                                {error.message}
+                                            </div>
+                                        )}
+
+                                        <button type="submit" class="btn primary-button login-button-shadow btn-block mb-4">
+                                            Sign up
+                                        </button>
+                                    </form>
+                                    <p class="">Already have an account?{" "}
+                                            <a href="login" class="small-link">
+                                                Login here
+                                            </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6 mb-5 mb-lg-0 signup-image-height">
+                            <img src="https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" class="signupcard-border w-100"
+                                alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" value={email} onChange={(event) => setEmail(event.target.value)} />
-                </div>
-                <div class="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input class="form-control" id="usernameInput" aria-describedby="usernameHelp" placeholder="Enter username" value={username} onChange={(event) => setUsername(event.target.value)} />
-                </div>
-                <div class="form-group">
-                    <label htmlFor="phonenum">Phone Number</label>
-                    <input class="form-control" id="phoneNumInput" aria-describedby="phonenumHelp" placeholder="Enter phone number" value={phoneNum} onChange={(event) => setPhoneNum(event.target.value)} />
-                </div>
-                <div class="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input class="form-control" id="passwordInput" aria-describedby="passwordHelp" placeholder="Enter password" value={password} onChange={(event) => setPassword(event.target.value)} />
-                </div>
-                <button class="btn btn-primary" type="submit">Sign Up</button>
-            </form>
-            {error && ( // Render the alert div if there is an error
-                <div className="alert alert-danger" role="alert">
-                    {error.message}
-                </div>
-            )}
+            </section>
         </div>
+
+
     );
 };
 
