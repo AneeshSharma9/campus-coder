d60bb7d3372e59f7ad69d4ad60b6e3c11d675899
AneeshSharma9 <ansh993@gmail.com>
2023-08-03 19:13:22 -0400

    styling homepage

--- src/App.js
+++ src/App.js
@@ -1,51 +1,198 @@
-import './App.css';
-import Navbar from './components/Navbar'
-import { Account } from './components/Account';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Account } from "./components/Account";
 
 function App() {
-  return (
-    <Account>
-      <div class="bgcolor">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
-        <Navbar />
-        <div class="container">
-          <div class= "">
-            <h1 class="title pt-5 mt-5">Campus Coder</h1>
-            <div class="my-5"></div>
-            <div class="main-button px-4 py-2 mr-3">
-              <a class="colorchange nav-link" href='#about'>About</a>
-            </div>
-            <div class="other-button px-4 py-2 mr-3">
-              <a class="colorchange-black nav-link" href="#how">How Does it Work?</a>
-            </div>
-            
-            <div class="" id="about">
-              <p class="introtext mt-5">
-                Introducing Campus Coder!<br /><br />
-                Are you a small business struggling to afford professionally done websites and automation scripts? Look no further!<br /><br />
-                Imagine having access to a talented pool of computer science students, eager to take on your projects at a fraction of the price.<br /><br />
-                Campus Coder is here to bridge the gap between small businesses and aspiring developers, creating a win-win situation for everyone involved!<br /><br />
-                With 99.9% of businesses in the US falling into the small business category, we understand the challenges you face in accessing affordable development solutions.<br /><br />
-                But fret not! Campus Coder is a revolutionary app that connects you with bright and passionate computer science students, hungry for real-world experience in their field.<br /><br />
-                By working with these talented individuals, you not only get access to cost-effective solutions, but you also help nurture the next generation of tech superstars!<br /><br />
-                With flexible timelines and a commitment to excellence, Campus Coder offers you the freedom to focus on what you do best, while talented students bring your visions to life.<br /><br />
-                Whether it's developing websites, creating automation scripts, or tackling small tasks, Campus Coder empowers both businesses and students alike!<br /><br />
-                So, join the vibrant community of visionaries and changemakers. Embrace innovation and unleash the power of collaboration with Campus Coder today!<br /><br />
-                Don't miss this incredible opportunity to access top-notch talent, save money, and make a positive impact on the lives of aspiring developers. Register for Campus Coder now!
-              </p>
-            </div>
-            
-          </div>
-          <h2 class="customh2">Why Campus Coder?</h2>
-          <h2 class="customh2" id="how">How Does it Work?</h2>
-          <h2 class="customh2">FAQ</h2>
-          <hr />
-        </div>
-      </div>
-    </Account>
+    return (
+        <Account>
+            <div class="bgcolor">
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+                    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                    crossOrigin="anonymous"
+                />
+                <Navbar />
+                <div class="image-container">
+                    <img
+                        src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80"
+                        class="w-100"
+                        alt=""
+                    />
+                    <span class="title">Campus Coder</span>
+                    <span class="tagline">Spark innovation together.</span>
+                    <div class="main-button homepage-button1">
+                        <a class="colorchange nav-link" href="#about">
+                            About
+                        </a>
+                    </div>
+                    <div class="other-button homepage-button2">
+                        <a class="colorchange-black nav-link" href="#how">
+                            How Does it Work?
+                        </a>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="" id="about">
+                        <p class="introtext mt-5">
+                        Campus Coder bridges businesses and young tech minds, sparking innovation and unleashing boundless possibilities. We empower small businesses by connecting them with talented computer science students, providing cost-effective solutions while nurturing the next generation of tech leaders. Join our vibrant community of visionaries and changemakers today!
+                        </p>
+                    </div>
 
-  );
+                    <h2 class="customh2">Why Campus Coder?</h2>
+                    <p class="introtext">
+                        Cost-Effective Solutions: Campus Coder offers a unique opportunity
+                        for small businesses to access top-notch development services
+                        without breaking the bank. By working with talented computer science
+                        students, you can benefit from cost-effective solutions tailored to
+                        your specific needs.
+                        <br />
+                        <br />
+                        Supporting Aspiring Developers: When you choose Campus Coder, you're
+                        not just getting a service; you're investing in the future of the
+                        tech industry. By collaborating with enthusiastic students, you play
+                        a crucial role in nurturing their skills and fostering their growth
+                        as the tech superstars of tomorrow.
+                        <br />
+                        <br />
+                        Freedom and Flexibility: We understand the challenges faced by small
+                        businesses, and that's why we offer flexible timelines and options.
+                        You can choose the best fit for your project and enjoy the freedom
+                        to focus on your core business activities.
+                        <br />
+                        <br />
+                        Empowering Collaboration: Campus Coder facilitates a vibrant
+                        community of businesses and students, encouraging collaboration and
+                        innovation. Together, we work towards creating solutions that drive
+                        positive change and impact.
+                        <br />
+                        <br />
+                    </p>
+
+                    <h2 class="customh2" id="how">
+                        How Does it Work?
+                    </h2>
+                    <p class="introtext">
+                        Business Registration: To get started with Campus Coder, small
+                        businesses need to register on our platform. The registration
+                        process is simple and requires basic information about your company
+                        and the type of project you need assistance with.
+                        <br />
+                        <br />
+                        Project Submission: After registering, businesses can submit their
+                        projects through the Campus Coder dashboard. Clearly outline the
+                        project's scope, requirements, and any specific skills you're
+                        looking for in a student developer.
+                        <br />
+                        <br />
+                        Student Matching: Once the project is submitted, our platform's
+                        intelligent matching algorithm identifies the most suitable student
+                        developers based on their skills and expertise. We consider factors
+                        such as programming languages, development experience, and past
+                        projects to ensure a perfect fit.
+                        <br />
+                        <br />
+                        Review and Selection: Businesses receive a list of potential student
+                        developers along with their profiles and project proposals. Review
+                        the proposals and choose the candidate that best aligns with your
+                        project vision.
+                        <br />
+                        <br />
+                        Project Collaboration: After selecting a student developer, you'll
+                        collaborate closely with them through our platform. You can discuss
+                        project details, set milestones, and communicate seamlessly
+                        throughout the development process.
+                        <br />
+                        <br />
+                        Development Phase: The student developer starts working on your
+                        project, utilizing their skills and creativity to bring your vision
+                        to life. They may seek your feedback at various stages to ensure the
+                        final product meets your expectations.
+                        <br />
+                        <br />
+                        Quality Assurance: While students are talented and passionate, we
+                        understand the importance of quality assurance. Our support team
+                        monitors projects to ensure they meet the desired standards,
+                        providing guidance and assistance to students when needed.
+                        <br />
+                        <br />
+                        Project Completion: Once the project is completed, you'll have the
+                        opportunity to review the deliverables and provide feedback. If
+                        everything meets your satisfaction, the project will be marked as
+                        complete.
+                        <br />
+                        <br />
+                        Feedback and Rating: We encourage businesses to provide feedback on
+                        their experience working with the student developer. This not only
+                        helps improve our platform but also serves as valuable input for the
+                        student's future endeavors.
+                        <br />
+                        <br />
+                        Long-Term Collaboration: If you had a positive experience with a
+                        particular student developer, you can choose to work with them on
+                        future projects or hire them for long-term development support.
+                        Campus Coder aims to build lasting connections between businesses
+                        and talented students.
+                        <br />
+                        <br />
+                    </p>
+
+                    <h2 class="customh2">FAQ</h2>
+                    <p class="introtext">
+                        1. How does Campus Coder ensure the quality of work delivered by
+                        students?
+                        <br />
+                        <br />
+                        We maintain a rigorous selection process for our student developers,
+                        ensuring they possess the necessary skills and expertise.
+                        Additionally, we encourage businesses to provide feedback on the
+                        projects, and our team offers support and guidance throughout the
+                        development process.
+                        <br />
+                        <br />
+                        2. What types of projects can I request on Campus Coder?
+                        <br />
+                        <br />
+                        Campus Coder offers a wide range of services, including website
+                        development, automation scripts, and tackling small tasks. Whatever
+                        your tech-related needs are, our talented students are eager to take
+                        on the challenge.
+                        <br />
+                        <br />
+                        3. How are projects priced on Campus Coder?
+                        <br />
+                        <br />
+                        Projects on Campus Coder are priced at a fraction of the cost of
+                        professional services. The exact pricing depends on the complexity
+                        and scope of the project. Our aim is to provide affordable solutions
+                        for small businesses while ensuring fair compensation for the
+                        students' efforts.
+                        <br />
+                        <br />
+                        4. Can I hire a student developer for a long-term project?
+                        <br />
+                        <br />
+                        Absolutely! Campus Coder welcomes both short-term and long-term
+                        projects. Whether you need a one-time task or ongoing development
+                        support, our students are ready to collaborate according to your
+                        project requirements.
+                        <br />
+                        <br />
+                        5. Is there any support available for businesses using Campus Coder?
+                        <br />
+                        <br />
+                        Yes, of course! Our dedicated support team is always available to
+                        assist you with any questions or concerns you may have. We're
+                        committed to ensuring a seamless experience for both businesses and
+                        student developers on our platform.
+                        <br />
+                        <br />
+                    </p>
+                    <hr />
+                </div>
+            </div>
+        </Account>
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import Pool from '../UserPool';
 import logo from '../public/campus_coder_logo_temp.png'
 
 function Navbar() {
+    const user = Pool.getCurrentUser();
     const getIdToken = () => {
         const user = Pool.getCurrentUser();
         if (user) {
@@ -22,7 +23,6 @@ function Navbar() {
 
     const [loginLabel, setLoginLabel] = useState('');
     const [loginhrefLabel, setLoginhrefLabel] = useState('');
-    const [requestMgmtLabel, setRequestMgmtLabel] = useState('');
 
     useEffect(() => {
         const fetchFullName = async () => {
@@ -34,11 +34,9 @@ function Navbar() {
                 let firstname = nameArray[0];
                 setLoginhrefLabel('profile')
                 setLoginLabel(firstname);
-                setRequestMgmtLabel('Manage Requests')
             } else {
                 setLoginhrefLabel('login')
                 setLoginLabel('Log In');
-                setRequestMgmtLabel('')
             }
         };
 
@@ -48,24 +46,24 @@ function Navbar() {
     return (
         <nav class="navbar navbar-expand navbar-light">
             <div class="pl-3">
-                <img src={logo} alt="Logo" class="" width={50}/>
+                <a href="/">
+                    <img src={logo} alt="Logo" class="" width={50} />
+                </a>
             </div>
-            
             <a class="logo ml-4" href="/">Campus Coder</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto mt-3">
-                    <li class="nav-item nav-item-style mr-4">
-                        <a class="navcolor nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                <ul class="navbar-nav ml-auto mt-2 mb-2">
+                    <li class="nav-item nav-item-style ">
+                        <a class="navcolor nav-link pr-4 pl-4" href="/">Home<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item nav-item-style">
+                        <a class="navcolor nav-link pr-4 pl-4" href="explore">Explore</a>
                     </li>
                     <li class="nav-item nav-item-style mr-4">
-                        <a class="navcolor nav-link" href="explore">Explore</a>
-                    </li>
-                    <li class="nav-item nav-item-style mr-4">
-                        <a class="navcolor nav-link" href="requestmgmt">{requestMgmtLabel}</a>
+                        {user ? (
+                            <a class="navcolor nav-link" href="requestmgmt">Manage Requests</a>
+                        ) : null}
                     </li>
                     <li class="login-button mr-4">
                         <a class="colorchange nav-link" href={loginhrefLabel}>{loginLabel}</a>
